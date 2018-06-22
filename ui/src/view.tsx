34c8ff8c4a5d61f35f6e3038dbb495f0655eaafe
pd <paolo.dellepiane@microarea.it>
2018-06-22 16:11:38 +0200

    .

--- ui/src/view.tsx
+++ ui/src/view.tsx
@@ -1,28 +1,19 @@
 import { h, View } from 'hyperapp';
 import State from './state';
 import { Actions } from './actions';
-import { Panel } from './panel';
-import { Textfield } from './textfield';
 
 export const view: View<State, Actions> = (state, actions) => (
   <main>
-    <Panel />
-    <Textfield path={state.filePath} text={state.text} oninput={e => actions.setText(e.target.value)} />
-    <br />
-    <div
-      style={{
-        display: 'flex',
-        backgroundColor: '#cccccc',
-        borderRadius: '5px',
-        padding: '20px 30px'
-      }}
-    >
-      <button class="ms-Button" onclick={_ => actions.getFile()}>
-        <span class="ms-Button-label">open</span>
-      </button>
-      <button class="ms-Button" onclick={_ => actions.setText(['', ''])}>
-        <span class="ms-Button-label">reset</span>
-      </button>
-    </div>
+    <section class="section">
+      <div class="container">
+        <textarea class="textarea" placeholder="10 lines of textarea" rows="10" />
+        <button style={{ margin: '5px 5px' }} class="button is-primary is-rounded" onclick={_ => actions.getFile()}>
+          open
+        </button>
+        <button style={{ margin: '5px 5px' }} class="button is-primary is-rounded" onclick={_ => actions.setText(['', ''])}>
+          reset
+        </button>
+      </div>
+    </section>
   </main>
 );
