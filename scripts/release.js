e2d4d27f090add28321088b22e6cd09344850ed7
pd <pd@pd.pd>
2018-10-25 20:28:52 +0200

    remove rm from release script

--- scripts/release.js
+++ scripts/release.js
@@ -2,8 +2,8 @@ const { execSync } = require('child_process');
 
 run('"./node_modules/.bin/rimraf" dist', '../ui');
 run('"./node_modules/.bin/parcel" build src/index.html --public-url ./', '../ui')
-run('"node_modules/.bin/inline-assets" dist/index.html ../src/d', '../ui')
-run('rm -rf src/d.sz', '../')
+run('"./node_modules/.bin/inline-assets" dist/index.html ../src/d', '../ui')
+run('"./node_modules/.bin/rimraf" ../src/d.sz')
 run('szip src/d', '../')
 run('cargo build --release', '../')
 
