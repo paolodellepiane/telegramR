36106dfdfa50e92e5e1811e82d7ee77d81417105
pd <paolo.dellepiane@microarea.it>
2018-11-16 17:08:00 +0100

    .

--- ui/src/actions.ts
+++ ui/src/actions.ts
@@ -3,18 +3,12 @@ import { ActionsType } from 'hyperapp';
 import { rpc } from './rpc';
 
 export class Actions implements ActionsType<State, Actions> {
-  hideNotificationLater = delay => setTimeout(() => this.hideNotification(), delay);
+  hideNotificationLater = delay => (_, actions: Actions) => setTimeout(actions.hideNotification, delay);
   hideNotification = () => ({ notificationShown: false });
   showNotification = (text: string) => ({ notificationShown: true, notification: text });
-  getFile = () => (state, actions: Actions) => {
-    rpc.invoke('getFile').then(actions.setText);
-    return state;
-  };
-  setText = (text: string[]) => state => ({ filePath: text[0], text: text[1] });
-  openDialog = () => state => {
-    rpc.invoke('openDialog').then(actions.getFile);
-    return state;
-  };
+  getFile = () => (_, actions: Actions) => rpc.invoke('getFile').then(actions.setText);
+  setText = (text: string[]) => ({ filePath: text[0], text: text[1] });
+  openDialog = () => rpc.invoke('openDialog').then(actions.getFile);
 }
 
 export const actions = new Actions();
