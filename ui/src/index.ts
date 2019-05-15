2697728c8485582f7c2b39810925974b5dec18f7
Paolo Dellepiane <paolo.dellepiane@microarea.it>
2019-05-15 16:03:51 +0200

    refactor

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -18,12 +18,13 @@ if (isIE) console.log('IE detected');
 if (process.env.NODE_ENV === 'production') withLogger(app)(initialState, actions, view, document.body);
 else {
   const config = { devtools: 'hyperapp-redux-devtools' };
-  let main;
-  if (!config.devtools || config.devtools === 'none') main = withLogger(app)(initialState, actions, view, document.body);
+  console.log('config:', config);
+  if (!config.devtools || config.devtools === 'none') withLogger(app)(initialState, actions, view, document.body);
   else if (isIE || config.devtools === 'hyperapp-devtools') {
     console.log('enable hyperapp-devtools');
-    import('hyperapp-devtools').then(devtools => (main = withLogger(devtools.default(app))(initialState, actions, view, document.body)));
+    import('hyperapp-devtools').then(devtools => withLogger(devtools.default(app))(initialState, actions, view, document.body));
   } else if (config.devtools === 'hyperapp-redux-devtools') {
+    console.log('enable hyperapp-redux-devtools');
     import('hyperapp-redux-devtools').then(devtools => withLogger(devtools.default(app))(initialState, actions, view, document.body));
   }
 }
